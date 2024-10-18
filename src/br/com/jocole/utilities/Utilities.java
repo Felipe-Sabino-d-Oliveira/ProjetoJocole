0fe9e4abf5f056068d15a4148ec39c847fe3339b
felipe.slima <fsabino006@gmail.com>
2024-10-18 17:25:14 -0300

    envio de hoje: base de vendas

--- src/br/com/jocole/utilities/Utilities.java
+++ src/br/com/jocole/utilities/Utilities.java
@@ -7,7 +7,6 @@ package br.com.jocole.utilities;
 import br.com.jocole.dal.ConnectionModule;
 import java.sql.Connection;
 import java.sql.Date;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
